60e3250d917879f0927530fdd4f04e3d8128a115
yassine-khadiri <yassinekhadiri77@gmail.com>
2023-01-18 15:42:57 +0100

    Add nginx conf && mariadb...

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,3 +7,13 @@ tar -xvzf latest.tar.gz
 mv wordpress /var/www/html/
 
 rm -f latest.tar.gz
+
+cp  /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+
+sed -i 's/database_name_here/$MYSQL_DATABASE/g' /var/www/html/wordpress/wp-config.php
+
+sed -i 's/username_here/$MYSQL_USER/g' /var/www/html/wordpress/wp-config.php
+
+sed -i 's/password_here/$MYSQL_PASSWORD/g' /var/www/html/wordpress/wp-config.php
+
+sed -i 's/localhost/$HOST_NAME/g' /var/www/html/wordpress/wp-config.php

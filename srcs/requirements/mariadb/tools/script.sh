05117acd33cc688d0c20ab224f017b1d03b0e3c3
yassine-khadiri <yassinekhadiri77@gmail.com>
2023-01-23 00:37:34 +0100

    Display The Wordpress Main Page, ...

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 service mysql start
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-                    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+                    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
                     FLUSH PRIVILEGES;"
+
+kill `cat /var/run/mysqld/mysqld.pid`
+exec "$@"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,14 +8,18 @@ mv wp-cli.phar /usr/local/bin/wp
 
 cd /var/www/html/
 
-wp core download
+wp core download --allow-root
 
-cp  /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+cp wp-config-sample.php wp-config.php
 
-sed -i 's/database_name_here/$MYSQL_DATABASE/g' /var/www/html/wordpress/wp-config.php
+sed -i '36 s/\/run\/php\/php7.3-fpm.sock/9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
-sed -i 's/username_here/$MYSQL_USER/g' /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 
-sed -i 's/password_here/$MYSQL_PASSWORD/g' /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
 
-sed -i 's/localhost/$HOST_NAME/g' /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
+
+sed -i "s/localhost/$HOST_NAME/g" wp-config.php
+
+exec "$@"
